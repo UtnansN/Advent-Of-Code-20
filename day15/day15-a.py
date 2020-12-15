afdc4e332c5ad1e5c86f8d4c3f67b734130a50d2
UtnansN <nauris.utnans@gmail.com>
2020-12-15 15:32:01 +0200

    Day 15 solutions

---
+++ day15/day15-a.py
@@ -0,0 +1,17 @@
+with open('input.txt') as f:
+    inputs = [int(x) for x in f.read().strip().split(',')]
+
+memory = {}
+for idx, input in enumerate(inputs):
+    memory[input] = idx + 1
+
+spoken = inputs[-1]
+for i in range(len(inputs), 2020):
+    try:
+        prev_spoken = memory[spoken]
+        memory[spoken] = i
+        spoken = i - prev_spoken
+    except KeyError:
+        memory[spoken] = i
+        spoken = 0
+print(spoken)
